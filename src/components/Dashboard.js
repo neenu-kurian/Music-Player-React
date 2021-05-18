29ec0361ba6e35957c2e61db6cc47c58bc000d47
Neenu Kurian <nkurian@salmon.com>
2021-05-18 23:08:29 +0200

    added albums, artits and playlists

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,12 +5,13 @@ import {
   } from '../actions/result';
 import Header from './Header';
 import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+// import { Redirect } from 'react-router-dom';
+import SearchResult from './SearchResult';
 
 
 const Dashboard = (props) => {
 
-	const { isValidSession, history } = props;
+	// const { isValidSession, history } = props;
 	const [selectedCategory, setSelectedCategory] = useState('albums');
 
 	const handleSearch = (searchTerm) => {
@@ -29,15 +30,26 @@ const Dashboard = (props) => {
 		//   });
 		// }
 	};
+	
+	const setCategory = (category) => {
+		setSelectedCategory(category);
+	};
 
- return (
+	const { albums, artists, playlist } = props;
+	const result = { albums, artists, playlist };
+
+ 	return (
 	<React.Fragment>
 	{/* {isValidSession() ? ( */}
 	  <div>
 		<Header />
 		<SearchForm handleSearch={handleSearch} />
 	
-	
+		<SearchResult
+            result={result}
+            setCategory={setCategory}
+            selectedCategory={selectedCategory}
+          />
 	  </div>
 	{/* ) : (
 	  <Redirect
--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import _ from 'lodash';
 import AlbumsList from './AlbumsList';
+import ArtistsList from './ArtistsList';
+import PlayList from './PlayList';
+
 
 const SearchResult = (props) => {
   const { result, setCategory, selectedCategory } = props;
@@ -41,6 +44,12 @@ const SearchResult = (props) => {
       </div>
       <div className={`${selectedCategory === 'albums' ? '' : 'hide'}`}>
         {albums && <AlbumsList albums={albums} />}
+      </div>
+      <div className={`${selectedCategory === 'artists' ? '' : 'hide'}`}>
+        {artists && <ArtistsList artists={artists} />}
+      </div>
+      <div className={`${selectedCategory === 'playlist' ? '' : 'hide'}`}>
+         {playlist && <PlayList playlist={playlist} />}
       </div>
     </React.Fragment>
   );
